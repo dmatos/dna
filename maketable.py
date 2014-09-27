9cf3443ceb0f4531f5a95c49d33cfe6cdaeb9fff
Diogo Matos <dmatos88@gmail.com>
2014-09-27 11:57:41 -0300

    new feature: creation of comparison table for genes in a pathway

--- maketable.py
+++ maketable.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 # Created in: Jun 19, 2014
-# Last modified in: Jun 19, 2014
+# Last modified in: Sep 27, 2014
 # Author: Diogo Matos <dmatos88@gmail.com>
 # Based on: create_tabley.py by Leandro Lima <llima@ime.usp.br>
 # Using KEGG REST python API: https://pypi.python.org/pypi/keggrest/0.1.1
@@ -38,8 +38,110 @@ def generateHeatmapColor(h, colorIndex):
     elif colorIndex == 7:       #7 orange CC0000
         color = '#CC'+2*h+'00'
     return color
+    
+'''
+Insert entries to both genes dictionary and kegg orthology dictionary given
+a species code and its genes on a specific pathway.
+
+genesDict    - Dicionario de genes {definition : {species_code : gene_code}}
+genes        - possiveis chaves que vao entrar
+species_code - codigo do organismo
+'''
+def makeGenesDict(genesDict, koDict, genes, species_code):
+    for gene in genes:
+        try: 
+            geneTemp = gene.split('\t')[1].split(':')[1]
+            defs = keggrest.RESTrequest('find/genes/'+geneTemp)
+            #print defs
+            defsSplited =  defs.split(';')                            
+            definition = defsSplited[1] 
+            if(definition.startswith(' K')):
+                definition = defsSplited[0].replace(gene.split('\t')[1], '')
+            #print definition                           
+            #definition = definition.split(' (')[0]          
+            
+            koList = keggrest.RESTrequest('link/ko/'+defsSplited[0]).split('\n')[0:-1]              
+            
+            koCode = koList[0].split('\t')[1]
+            if (koCode not in koDict):
+                koDict[koCode] = definition
+                genesDict[koCode] = {}
+            
+            genesDict[koCode][species_code] = gene.split('\t')[1]
+            
+        except:
+            print 'NO DEFINITION FOUND FOR ' + gene + ' IN ' + species_code
+            
+'''
+Creates comparison table of organisms for a given pathway.
+
+pathway         - pathway code
+organismsNames  - list of organisms names
+organisms       - list of abreviations of organisms
+'''    
+def pathwayComparisonTable(pathway, organismsNames, organisms):
+    print 'creating page for '+pathway
+    htmlFileName = pathway + '_comparison_table.html'
+    html = open('../'+htmlFileName, 'w')
+    html.write('<html><head><title>Pathway %s</title></head>\n<body>\n' % (pathway))
+    html.write('<h1 > <a href="http://www.genome.jp/kegg/pathway/map/map%s.html"> Pathway %s </a> </h1>' % (pathway, pathway))
+    html.write('<table border="1">\n')
+    html.write('<tr><td rowspan="2"><center>Genes</center></td>\n')  
+    
+    organismsCounter = []
+    for i in range(len(organismsNames)):
+        count = 0
+        #print organismsNames[i]
+        for org in organisms:
+            if org.find(organismsNames[i]) >= 0:                                
+                count = count + 1
+                #print org
+        #print count
+        organismsCounter.append(count)
+        html.write('<td colspan="%d" bgcolor="%s">%s</td>\n' % (count, colors[i], organismsNames[i]))
+    html.write('</tr>\n') 
+    
+    #dictionary ko -> species_code -> gene_code
+    genesDict = {}
+    #dictionary ko -> definition
+    koDict = {}
+
+    #print pathway
+    colorCount = 0
+    leftCounter = 0
+    for i in range(len(organisms)):   
+        species_code = organisms[i][14:17]
+        if i == organismsCounter[colorCount] + leftCounter: 
+                colorCount = colorCount + 1
+                leftCounter = i
+        #print colorCount
+        color = colors[colorCount]
+        indexOfDefinition = str(organisms[i]).find(';')
+        html.write('<td bgcolor="%s"><center><a href="http://www.kegg.jp/kegg-bin/show_organism?org=%s" title="%s">%s</a> <br /><font size=1>%d</font></center></td>\n' % (color, species_code, organisms[i][indexOfDefinition+1], species_code, len(keggrest.RESTrequest('list/'+species_code).split('\n'))-1))
+        #print species_code
+        genes = keggrest.RESTrequest('link/genes/'+species_code+pathway).split('\n')[0:-1] 
+        #print genes                               
+        if (len(genes) > 0 and len(genes[0]) > 0):
+            makeGenesDict(genesDict, koDict, genes, species_code)
+    html.write('</tr>\n')
+            
+    #dictionaries ares complete, creating table of comparison
+    for key in genesDict.keys():
+        #print key
+        html.write('<tr>\n')           
+        html.write('<td>%s</td>' % (koDict[key]))
+        for i in range(len(organisms)):
+            species_code = organisms[i][14:17]
+            if species_code in genesDict[key]:
+                html.write('<td><a href="http://www.genome.jp/dbget-bin/www_bget?%s">%s</a></td>' % (genesDict[key][species_code],genesDict[key][species_code]))
+            else:
+                html.write('<td><center>-</center></td>')     
+        html.write('</tr>\n') 
+    html.write('</table>\n') 
+    html.write('</body>\n') 
+    html.write('</html>\n') 
         
-def create_page(species_code, genes, pathway, create_html=False):
+def create_genes_def_page(species_code, genes, pathway, create_html=False):
     filename = 'genes_of_' + species_code + '_in_' + pathway + '.html'    
     #print 'creating page ' + filename
     if create_html:
@@ -60,7 +162,7 @@ def create_page(species_code, genes, pathway, create_html=False):
             except:
                 page.write('<li>%s - NO DEFINITION FOUND <a href="http://www.genome.jp/dbget-bin/www_bget?%s">%s</a></li>\n' % (gene, gene, gene))
         page.write('  </ul>\n')
-        page.write('<br /><a href="output.html">Go back</a><br />\n')
+        page.write('<br /><a href="index.html">Go back</a><br />\n')
         page.write('</body></html>')
         page.close()
     return filename
@@ -139,7 +241,7 @@ def pathways_cross_organisms(pathwaysLen, organismsLen, htmlFileName, link, args
         html.write('<td bgcolor="%s"><center><a href="http://www.kegg.jp/kegg-bin/show_organism?org=%s" title="%s">%s</a> <br /><font size=1>%d</font></center></td>\n' % (color, org_id, organisms[i][indexOfDefinition+1], org_id, len(keggrest.RESTrequest('list/'+org_id).split('\n'))-1))    
     html.write('</tr>\n')
     for pathway in pathways:
-        html.write('<tr>\n<td><a href="http://www.genome.jp/kegg/pathway/map/map%s.html" title="%s">%s</a> <font size=1>%d</font></td>\n' % (pathway, pathways_names[pathway], pathway, number_of_enzymes[pathway]))
+        html.write('<tr>\n<td><a href="%s_comparison_table.html" title="%s">%s</a> <font size=1>%d</font></td>\n' % (pathway, pathways_names[pathway], pathway, number_of_enzymes[pathway]))
         #print pathway
         colorCount = 0
         leftCounter = 0
@@ -157,12 +259,14 @@ def pathways_cross_organisms(pathwaysLen, organismsLen, htmlFileName, link, args
             else:
                 color = colors[colorCount]
             if (len(genes) > 0 and len(genes[0]) > 0):
-                pagename = create_page(species_code, genes, pathway, True)
+                pagename = create_genes_def_page(species_code, genes, pathway, True)
                 html.write('<td bgcolor="%s"><center><a href="%s">%d</a></center></td>' % (color, pagename, len(genes)))
             else:
                 html.write('<td bgcolor="%s"><center>%d</center></td>' % (color, 0))
 
         html.write('</tr>\n')
+        #writes pathway comparison table page
+        pathwayComparisonTable(pathway, organismsNames, organisms)
     
     if link=='commoncolors.html':
         html.write('</table>\n<br /><a href="%s">See common colors</a>\n</body>\n</html>\n' % (link))
